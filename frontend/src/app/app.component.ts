b02955e89e071f5505a853d7bdeecf694ee6c047
Simone <s.ingenito02@gmail.com>
2024-06-29 16:44:16 +0200

    controlli input all'interno dei form

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { HomepageComponent } from './Components/homepage/homepage.component';
 import { FooterComponent } from './Components/footer/footer.component';
@@ -10,7 +11,7 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, 
+  imports: [CommonModule, RouterOutlet, NavbarComponent, 
     HomepageComponent, FooterComponent, 
     LoginComponent, SignupComponent],
   templateUrl: './app.component.html',
